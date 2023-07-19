c423b4b33cee0d11b766be5b8ae0d061247164d8
Rokeya yasmin <135530632+Rokeya0@users.noreply.github.com>
2023-07-19 23:18:39 +0600

    Add files via upload

---
+++ model.dart
@@ -0,0 +1,5 @@
+final List<String> newsArticles = [
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsPNGFbC7MFd81stxAigo3DcOS5t42WHoBgQ&usqp=CAU',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsPNGFbC7MFd81stxAigo3DcOS5t42WHoBgQ&usqp=CAU',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsPNGFbC7MFd81stxAigo3DcOS5t42WHoBgQ&usqp=CAU',
+];
